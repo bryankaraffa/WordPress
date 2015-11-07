a9685e76a0327baf91305c165a00e76b2b3e3179
Bryan Karaffa <bryankaraffa@gmail.com>
2015-11-06 17:03:46 -0800

    Matched the db.php to the WordPress default values

--- config/db.php
+++ config/db.php
@@ -2,6 +2,6 @@
 //RightScale PHP App Database Connection Information
 $hostname_DB = "localhost";
 $database_DB = "wordpress";
-$username_DB = "root";
-$password_DB = "mysql_password";
+$username_DB = "username";
+$password_DB = "password";
 ?>
